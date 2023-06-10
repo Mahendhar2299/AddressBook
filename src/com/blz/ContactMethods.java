8999a0ce85018a8d6bedf6435aeee33a7d0b0f28
Mahendhar2299 <delphic22mahi@gmail.com>
2023-06-10 17:31:54 +0530

    Refactored the code to edit the saved contact

--- src/com/blz/ContactMethods.java
+++ src/com/blz/ContactMethods.java
@@ -43,4 +43,79 @@ public class ContactMethods {
             System.out.println(person.toString());
         }
     }
+    public void EditContact() {
+        System.out.println("\nEnter first name to edit :- ");
+        String name = scanner.next();
+
+        for(Contact person : contactBook) {
+            if(name.equals(person.First_Name)) {
+                System.out.print("\nSelect option to edit----\n1.First_name \n2.Last_name \n3.Address \n4.City \n5.State \n6.Zip_code \n7.Phone_number \n8.Email :- ");
+                int option = scanner.nextInt();
+                switch(option) {
+                    case 1:
+                        System.out.print("Enter new first name :- ");
+                        String newFirstName = scanner.next();
+                        person.setFirst_Name(newFirstName);
+                        System.out.println("First name is updated.");
+                        break;
+
+                    case 2:
+                        System.out.print("Enter new last name :- ");
+                        String newLastName = scanner.next();
+                        person.setLast_Name(newLastName);
+                        System.out.println("Last name is updated.");
+                        break;
+
+                    case 3:
+                        System.out.print("Enter new Address :- ");
+                        String newAddress = scanner.next();
+                        person.setAddress(newAddress);
+                        System.out.println("Address is updated.");
+                        break;
+
+                    case 4:
+                        System.out.print("Enter new city name :- ");
+                        String newCity = scanner.next();
+                        person.setCity(newCity);
+                        System.out.println("City is updated.");
+                        break;
+
+                    case 5:
+                        System.out.print("Enter new state name :- ");
+                        String newState = scanner.next();
+                        person.setState(newState);
+                        System.out.println("State is updated.");
+                        break;
+
+                    case 6:
+                        System.out.print("Enter new Zip code :- ");
+                        int newZip = scanner.nextInt();
+                        person.setZip_Code(newZip);
+                        System.out.println("Zip code is updated.");
+                        break;
+
+                    case 7:
+                        System.out.print("Enter new phone number :- ");
+                        int newPhone = scanner.nextInt();
+                        person.setPhone_Number(newPhone);
+                        System.out.println("Phone number is updated.");
+                        break;
+
+                    case 8:
+                        System.out.print("Enter new email :- ");
+                        String newEmail = scanner.next();
+                        person.setEmail(newEmail);
+                        System.out.println("Email is updated.");
+                        break;
+
+                    default:
+                        System.out.println("Please enter a number between 1 to 8 only...");
+
+                }
+            }
+            else {
+                continue;
+            }
+        }
+    }
 }
--- src/com/blz/Main.java
+++ src/com/blz/Main.java
@@ -1,11 +1,28 @@
 package com.blz;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book ");
-        ContactMethods.addContact();
-        ContactMethods.displayContacts();
+        ContactMethods obj = new ContactMethods();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("--------------------Welcome To Address Book Program-----------------------");
+        while(true) {
+            System.out.println("Select the options\n 1. Add Contact\n 2. Display Object\n 3. Edit Existing Contact\n 4.Exit");
+            int choice = scan.nextInt();
+            switch (choice) {
+                case 1 : obj.addContact();
+                    break;
+                case 2 : obj.displayContacts();
+                    break;
+                case 3 : obj.EditContact();
+                    break;
+                case 4 : return;
+                default:System.out.println("Enter Valid input");
+            }
+            scan.close();
+        }
     }
 }
